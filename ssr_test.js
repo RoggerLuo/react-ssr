92856de6cfa9ceb359e333e179fddc5fcabf866b
Roger <luojie.5408@163.com>
2019-10-16 15:50:39 +0800

    update from air

---
+++ ssr_test.js
@@ -0,0 +1,39 @@
+import express from "express";
+import path from "path";
+
+import React from "react";
+import { renderToString } from "react-dom/server";
+// import Layout from "./components/Layout";
+const Layout = function(){
+    return (<div>abc</div>)
+}
+const app = express();
+
+app.use( express.static( path.resolve( __dirname, "../dist" ) ) );
+
+app.get( "/*", ( req, res ) => {
+    const jsx = ( <Layout /> );
+    const reactDom = renderToString( jsx );
+
+    res.writeHead( 200, { "Content-Type": "text/html" } );
+    res.end( htmlTemplate( reactDom ) );
+} );
+
+app.listen( 2048 );
+console.log('listen( 2048 )')
+function htmlTemplate( reactDom ) {
+    return `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="utf-8">
+            <title>React SSR</title>
+        </head>
+        
+        <body>
+            <div id="app">${ reactDom }</div>
+            <script src="./app.bundle.js"></script>
+        </body>
+        </html>
+    `;
+}
